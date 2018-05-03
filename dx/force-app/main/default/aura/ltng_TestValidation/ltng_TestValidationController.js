68749131a77ed59225e2a2c796e7f218882f96b5
Thomas Zarr <tzarr@salesforce.com>
2018-05-03 13:48:27 -0600

    partial: making a simple demo around the disabled behavior of fields

--- dx/force-app/main/default/aura/ltng_TestValidation/ltng_TestValidationController.js
+++ dx/force-app/main/default/aura/ltng_TestValidation/ltng_TestValidationController.js
@@ -1,5 +1,22 @@
 ({
-	myAction : function(component, event, helper) {
-		
-	}
+	init : function(component, event, helper) {
+        console.info('init ran');
+        
+		/*
+		var options = [
+            { value: "new", label: "New" },
+            { value: "in-progress", label: "In Progress" },
+            { value: "finished", label: "Finished" }
+        ];
+        component.set("v.comboOptions", options);
+        */
+
+        //Load initial record
+        //helper.loadRecord(component, helper);
+
+        $A.util.addClass(component.find('level2'), 'slds-is-disabled');
+        $A.util.addClass(component.find('level2'), 'JABRA');
+
+        component.set('v.level2Classes', 'custom-disabled');
+	},
 })
